933a6f8e21cdd16d082ae18322b3f1f2530118b9
arfanroky-github <arfanroky0@gmail.com>
2023-06-01 22:40:19 +0600

    [DONE] create user interface and model

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+import { IUser } from './user.interface';
+
+type UserModel = mongoose.Model<IUser, object >
+
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model<IUser, UserModel>('User', userSchema);
+
+export default User;
